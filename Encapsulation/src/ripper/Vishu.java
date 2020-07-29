01682fd1baeacf6ddea8416851b449304e3b1015
LAPTOP-J2A6VURN\rajin <neelimayadav127@gmail.com>
2020-07-29 18:46:28 -0500

    Encapsulation completed

---
+++ Encapsulation/src/ripper/Vishu.java
@@ -0,0 +1,14 @@
+package ripper;
+
+public class Vishu
+{
+
+	public static void main(String[] args)
+	{
+		Vrishank v=new Vrishank();
+		         v.type1();
+		         v.type2();
+
+	}
+
+}
---
+++ Encapsulation/src/ripper/Vrishank.java
@@ -0,0 +1,16 @@
+package ripper;
+
+public class Vrishank 
+{
+   private int g=5;
+   private String name="dinku";
+   
+   public void type1()
+   {
+	   System.out.println("employee id is "+g);
+   }
+   public void type2()
+   {
+	   System.out.println("employer name is "+name);
+   }
+}
